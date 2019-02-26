ce1af3d2a6ee408452841a76d7088d1bbd118df7
Cy <cy@Cys-MacBook-Pro.local>
2019-02-26 15:27:04 -0500

    new video layers

--- keyboardVideo/src/ofApp.cpp
+++ keyboardVideo/src/ofApp.cpp
@@ -6,40 +6,72 @@ int a=0;
 int a2=0;
 //--------------------------------------------------------------
 void ofApp::setup(){
+    ofSetBackgroundColor(0);
     video.load("fractal.mp4");
     video_two.load("fractal.mp4");
     video.setVolume(0.1);
+    
+    mVidGrabber.setVerbose(true);
+    mVidGrabber.setup(160,120);
+    screenShot.load("screenShot.png");
+
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     video.update();
     video_two.update();
-    
+    mVidGrabber.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    int pattern_width=160;
+int pattern_width=160;
     int pattern_height=120;
-    int width=240;
-    int height=240;
-    ofSetColor(r,g,b);
-    for(int i = 0; i < ofGetScreenHeight(); i += pattern_height){
-        for(int j = 0; j < ofGetScreenWidth(); j += pattern_width){
-            video.draw(j, i, pattern_width, pattern_height);
+    int width=160;
+    int height=120;
+    
+    ofPushMatrix();
+    for(int c = 0; c < ofGetWindowWidth(); c += width/2){
+        for(int b = 0; b < ofGetWindowHeight(); b += height/2)
+        {
+            ofSetColor(200, 0, 255,255);
+            mVidGrabber.draw(c,b,width/2,height/2);
+        }
+        
+    }
+    ofPopMatrix();
+
+    ofPushMatrix();
+    ofSetColor(r,g,b,100);
+    for(int j = 0; j < ofGetWindowWidth(); j += width) {
+        for(int i = 0; i < ofGetWindowHeight(); i += height){
+            video.draw(j, i, width, height);
         }
     }
+    ofPopMatrix();
     ofPushMatrix();
-    for(int y = 0; y < ofGetScreenHeight(); y += height*2){
-        for(int x = 0; x < ofGetScreenWidth(); x += width*2){
+    for(int c = 0; c < ofGetWindowWidth(); c += width*2){
+        for(int b = 0; b < ofGetWindowHeight(); b += height*2)
+        {
+    ofSetColor(100, 255, 0,a);
+    mVidGrabber.draw(c,b,width*2,height*2);
+        }
+        
+    }
+    ofPopMatrix();
+    
+    ofPushMatrix();
+    for(int x = 0; x < ofGetWindowWidth(); x += width*3) {
+        for(int y = 0; y < ofGetWindowHeight(); y += height*3){
             ofSetColor(r,g,b,a2);
             //ofSetColor(r,0,b,a2);
-            video_two.draw(x, y,width,height);
+            video_two.draw(x, y,width*3,height*3);
             //ofRect(x, y,width,height);
-            ofPopMatrix();
         }
     }
+    ofPopMatrix();
+
 }
 
 //--------------------------------------------------------------
@@ -62,9 +94,10 @@ void ofApp::keyPressed(int key){
             video_two.stop();
             break;
         case 'h':
-            a=255;
-            a2=200;
+            a=100;
+            a2=100;
             break;
+            
     }
 }
 
@@ -76,6 +109,7 @@ void ofApp::keyReleased(int key){
             a2=0;
             break;
     }
+   
     
 }
 
@@ -91,11 +125,12 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
+    screenShot.grabScreen(0,0,ofGetWindowWidth(),ofGetWindowHeight());
+    screenShot.save("screenShot.png");
     
 }
 
--- keyboardVideo/src/ofApp.h
+++ keyboardVideo/src/ofApp.h
@@ -20,6 +20,9 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
+    ofVideoGrabber mVidGrabber;
     ofVideoPlayer video;
     ofVideoPlayer video_two;
+    ofImage screenShot;
 };
